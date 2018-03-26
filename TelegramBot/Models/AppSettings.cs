653e368cb0c8d9bfe4dadc3806047cbce0876f1d
Forichok <Forichok@icloud.com>
2018-03-27 02:24:11 +0300

    Add project files.

---
+++ TelegramBot/Models/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBot.Models
+{
+   public static class AppSettings
+   {
+       public static string url { get; set; } = "";
+
+       public static string name { get; set; } = "ForichokBot";
+
+       public static string key { get; set; } = "555110319:AAHUPry0-tV0hQXy7g-jruIXOlsrIu_vwuY";
+   }
+}

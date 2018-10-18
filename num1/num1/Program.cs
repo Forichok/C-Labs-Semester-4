51472b918b38c2bdb63c766768eb571b179dd8cd
Forichok <nnudalov@gmail.com>
2018-10-18 23:21:34 +0300

    Add files via upload

---
+++ num1/num1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex[] karanoTest = Solver.Kordan(1, 2, -3, 5);
+            Complex[] vietaTest = Solver.Vieta(1, 2, -3, 5);
+            
+            Console.ReadKey();
+        }
+    }
+}

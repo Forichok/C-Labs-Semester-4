5dd0abb4e899728aa6c7b23bc53e13d26b8e1b26
Forichok <nnudalov@gmail.com>
2018-10-18 23:16:57 +0300

    Add files via upload

---
+++ LAB2/LAB2/GetTypeInfo.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace LAB2
+{
+    class GetTypeInfo
+    {
+        private Type _type;
+
+        public GetTypeInfo(string type, bool throwOnErorr = true, bool ignoreCase = true)
+        {
+
+            try
+            {
+                _type = Type.GetType(type, throwOnErorr, ignoreCase);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Incorrect type name");
+                
+            }
+        }
+
+        public object InvokeMethod(string name, string[] args = null)
+        {
+            object item = new object();
+            var methods = _type.GetMethods();
+            foreach (var method in methods)
+            {
+                if (method.Name.Contains(name))
+                {
+                    item = Activator.CreateInstance(_type);
+
+                    if (method.GetParameters().Length == args.Length)
+                    {
+                        try
+                        {
+                            var newArgs = ChangeParameters(method.GetParameters(), args);
+                            return method.Invoke(item, newArgs);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+                    }
+                }
+            }
+            return item;
+        }
+
+        public object InvokeMethod(string name,object[] args=null)
+        {
+            object item=new object();
+            var methods = _type.GetMethods();
+            foreach (var method in methods)
+            {
+                if (method.Name.Contains(name))
+                {
+                    item = Activator.CreateInstance(_type);
+                    if(method.GetParameters().Length==args.Length)
+                        try
+                        {
+                            return method.Invoke(item, args);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+                }
+            }
+            return item;
+        }
+
+        private object[] ChangeParameters(ParameterInfo[] parametersInfo, string[] parameters)
+        {
+            if (parametersInfo.Length != parameters.Length) return null;
+
+            List<object> objParameters = new List<object>();
+
+            for (int i = 0; i < parametersInfo.Length; i++)
+            {
+                try
+                {
+                    objParameters.Add(Convert.ChangeType(parameters[i], parametersInfo[i].ParameterType));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            return objParameters.ToArray();
+        }
+
+        public string GetInfo()
+        {
+            if (_type == null) return "error";
+            string output = String.Empty;
+            output += "Type Name:" + _type.Name + "\n";
+            output += "Interfaces:\n";
+            output += GetPartsInfo(_type.GetInterfaces());           
+            output += "Constructors:\n";
+            output += GetPartsInfo(_type.GetConstructors());
+            output += "Fields:\n";
+            output += GetPartsInfo(_type.GetFields());
+            output += "Members:\n";
+            output += GetPartsInfo(_type.GetMembers());
+            output += "Methods:\n";
+            output += GetPartsInfo(_type.GetMethods());
+            output += "Events:\n";
+            output += GetPartsInfo(_type.GetEvents());
+
+            return output;
+        }
+
+        private string GetPartsInfo(object[] objs)
+        {
+            string output= String.Empty;
+
+            foreach (var obj in objs)
+            {
+                output += obj+"\n";
+            }
+            return output;
+        }
+
+        public object Create()
+        {
+            object item=null;
+
+            try
+            {
+                 item = Activator.CreateInstance(_type);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There is no default constructor");
+            }
+            return item;
+        }
+
+    }
+}
---
+++ LAB2/LAB2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LAB2;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetTypeInfo test = new GetTypeInfo("System.Text.StringBuilder");
+            
+            var a = test.Create();
+            Console.WriteLine(a.GetType());
+
+            Console.WriteLine(test.GetInfo());
+
+            Console.WriteLine( test.InvokeMethod("Insert",new[]{"0","d"}));
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
